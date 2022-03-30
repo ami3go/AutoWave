c6bb09a4cc4fbe9f91b70d878f400f6dc1db1c39
Aleksandr Chasnyk <alexandr.chasnyk@yandex.com>
2022-03-30 16:58:55 +0300

    added waveform B support

--- Examples/Autowave_class_test.py
+++ Examples/Autowave_class_test.py
@@ -1,8 +1,8 @@
-import AutoWave_class
+import src.AutoWave_class as AutoWave_class
 import time
-import Timer_class
+import src.Timer_class as Timer_class
 
-timer = Timer_class.Timer()
+# timer = Timer_class.Timer()
 
 
 
@@ -10,28 +10,28 @@ timer = Timer_class.Timer()
 inst_folder = "/home/guest/DowFiles"
 
 WaveFormA = [
-            "JLC_CI256_WFA_20000ms.dpt",
-            "JLR_CI265_WFA_1000ms.dpt",
-            "JLR_CI265_WFA_200ms.dpt",
-            "JLR_CI265_WFA_100ms.dpt",
-            "JLR_CI265_WFA_60ms.dpt",
-            "JLR_CI265_WFA_48ms.dpt",
-            "JLR_CI265_WFA_35ms.dpt",
-            "JLR_CI265_WFA_24ms.dpt",
-            "JLR_CI265_WFA_18ms.dpt",
-            "JLR_CI265_WFA_12ms.dpt",
-            "JLR_CI265_WFA_9ms.dpt",
-            "JLR_CI265_WFA_6ms.dpt",
-            "JLR_CI265_WFA_3ms.dpt",
+            # "JLC_CI256_WFA_20000ms.dpt",
+            # "JLR_CI265_WFA_1000ms.dpt",
+            # "JLR_CI265_WFA_200ms.dpt",
+            # "JLR_CI265_WFA_100ms.dpt",
+            # "JLR_CI265_WFA_60ms.dpt",
+            # "JLR_CI265_WFA_48ms.dpt",
+            # "JLR_CI265_WFA_35ms.dpt",
+            # "JLR_CI265_WFA_24ms.dpt",
+            # "JLR_CI265_WFA_18ms.dpt",
+            # "JLR_CI265_WFA_12ms.dpt",
+            # "JLR_CI265_WFA_9ms.dpt",
+            # "JLR_CI265_WFA_6ms.dpt",
+            # "JLR_CI265_WFA_3ms.dpt",
+            "JLR_CI265_WFA_PB.dpt",
 ]
 
 
 inst = AutoWave_class.com_interface()
-
+inst.
 
 for test in WaveFormA:
     inst.init()
-    inst
     test_time = inst.get_test_time(test)
     timer.start(round(test_time)+20)
     time.sleep(1)
